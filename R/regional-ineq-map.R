6fc55193f95568a66bbf0253d39db586e063c732
bstacherl <barbara.stacherl@gmx.at>
2018-12-29 19:10:08 +0100

    Spanien Landkarte regionale per capita GDP auf NUTS3 Ebene geplottet

---
+++ R/regional-ineq-map.R
@@ -0,0 +1,67 @@
+rm(list=ls())
+
+# working directory muss in ineq_project sein
+temp <- tempfile(fileext = ".zip")
+download.file("http://ec.europa.eu/eurostat/cache/GISCO/distribution/v2/nuts/download/ref-nuts-2013-03m.shp.zip", temp)
+outDir<-"./data"
+unzip(temp, exdir=outDir)
+library(rgdal)
+zipF <- list.files(path = "./data", pattern=".zip", full.names = T)
+sapply(zipF, unzip, exdir = outDir)
+list.files(path="./data", pattern = 'NUTS_RG_03M_2013.*.shp$')
+shp <- readOGR(dsn = "./data", layer ="NUTS_RG_03M_2013_4326_LEVL_3", encoding = "UTF-8")
+shp <- spTransform(shp, CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs"))
+plot(shp)
+spain <- shp[grepl("ES", shp[["NUTS_ID"]]), ]
+spain <- as.character(spain$NUTS_ID)
+islands <- c("ES703", "ES704", "ES705", "ES706", "ES707", "ES708", "ES709", "ES531", "ES532", "ES533")
+spain <- setdiff(spain, islands)
+shp <- shp[ shp$NUTS_ID %in% spain, ]
+plot(shp)
+
+library(xlsx)
+library(openxlsx)
+library(readxl)
+library(XML)
+
+# download.file("http://appsso.eurostat.ec.europa.eu/nui/download?p=4c4dc5f0-0971-4b24-b5fa-cd91af619cca-1546102236507_&_=1546103337998", destfile = "./data/gdp.xls")
+# does not work to open in R directly
+# downloaded xls file manually into data 
+
+gdp <- read_xls("./data/gdp.xls", col_names = TRUE, skip = 8)
+gdp <- gdp[grepl("ES", gdp[["GEO/TIME"]]), ]
+gdp <- gdp[, -which(names(gdp) == "2016")]
+names(gdp)[1]<-paste("NUTS_ID")
+gdp <- gdp[ gdp$NUTS_ID %in% spain, ]
+# gdp for NUTS 3 regions (in million €)
+
+pop <- read_xls("./data/population.xls", col_names = TRUE, skip = 8)
+names(pop)[1]<-paste("NUTS_ID")
+pop <- pop[ pop$NUTS_ID %in% spain, ]
+
+pop$`2000` <- as.numeric(pop$`2000`)
+pop$`2001` <- as.numeric(pop$`2001`)
+pop$`2002` <- as.numeric(pop$`2002`)
+pop$`2003` <- as.numeric(pop$`2003`)
+pop$`2004` <- as.numeric(pop$`2004`)
+pop$`2005` <- as.numeric(pop$`2005`)
+pop$`2006` <- as.numeric(pop$`2006`)
+pop$`2007` <- as.numeric(pop$`2007`)
+gdp$`2000` <- as.numeric(gdp$`2000`)
+
+library(tibble)
+gdp <- column_to_rownames(gdp, var = "NUTS_ID")
+pop <- column_to_rownames(pop, var = "NUTS_ID")
+gdp <- gdp*1000000
+pop <- pop * 1000
+pc_gdp <- gdp / pop
+
+pcgdp15 <- rownames_to_column(pc_gdp, var = "NUTS_ID")
+myvars <- c("NUTS_ID", "2015")
+pcgdp15 <- pcgdp15[myvars]
+names(pcgdp15)[2]<-paste("pcgdp_15")
+
+shp <- merge(shp, pcgdp15, all.x = TRUE, all.y = TRUE, by.x = "NUTS_ID", by.y = "NUTS_ID") 
+library(GISTools)
+shades <- auto.shading(shp$pcgdp_15)
+choropleth(shp, shp$pcgdp_15, shades)
